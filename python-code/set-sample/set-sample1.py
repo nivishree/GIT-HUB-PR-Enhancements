7cba6b6bd05bd953951b387d9e2a77c8f633e344
Nivishree Palvannan <nivipalg@gmail.com>
2022-08-30 13:30:27 -0400

    Update set-sample1.py

--- python-code/set-sample/set-sample1.py
+++ python-code/set-sample/set-sample1.py
@@ -1,3 +1,8 @@
-print("This is a test file")   
 import os
-value = 10
+import math
+import sys 
+Value = 1 
+Value = 2 
+addedValue = 1 + 2 
+print(addedValue) 
+print("this is a simple test code")
