453bbb088ce45f9de48f001a1cc09d8667ba3873
Nivishree Palvannan <nivipalg@gmail.com>
2022-08-30 12:26:41 -0400

    Update set-sample1.py

--- python-code/set-sample/set-sample1.py
+++ python-code/set-sample/set-sample1.py
@@ -1,7 +1,9 @@
+print("this is test print statement")  
+value = 50 
 import os
-import math
 value = 20 
 value2 = 40 
+import math
 Total = value + value2 
 print(value) 
 print("this is test print statement")  
